6fa38d1e3faf73f2aaca62ecb0c973a3c03e07b1
Evelyn Mylena <48720042+EvyMylena@users.noreply.github.com>
2019-08-19 15:28:52 -0300

    Create Application.java

---
+++ Entrega-1/Application.java
@@ -0,0 +1,16 @@
+public class Application {
+	public static void main (String[] args) {
+		HelloWorld helloWorld=new HelloWorld();
+		helloWorld.setName("Evelyn");
+		helloWorld.imprimir();
+		HelloWorld helloWorld2=new HelloWorld();
+		helloWorld2.setName("Mylena");
+		helloWorld2.imprimir();
+		HelloWorld helloWorld3=new HelloWorld();
+		helloWorld3.setName("João");
+		helloWorld3.imprimir();
+		HelloWorld helloWorld4=new HelloWorld();
+		helloWorld4.imprimir();
+	}
+
+}
