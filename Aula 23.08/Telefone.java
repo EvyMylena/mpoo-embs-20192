bf0c437fc7fbc3a6ead93a6426b053be40b22188
Evelyn Mylena <48720042+EvyMylena@users.noreply.github.com>
2019-08-23 09:43:45 -0300

    Create Telefone.java

---
+++ Aula 23.08/Telefone.java
@@ -0,0 +1,25 @@
+package meuprojeto;
+
+public class Telefone {
+	private int ddd;
+	private int numero;
+	private TipoTelefone tipo;
+	public int getDdd() {
+		return ddd;
+	}
+	public void setDdd(int ddd) {
+		this.ddd = ddd;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public TipoTelefone getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoTelefone tipo) {
+		this.tipo = tipo;
+	}
+}
