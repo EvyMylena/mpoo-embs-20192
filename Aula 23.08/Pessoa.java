93d0f93b1c52db22e68c1ffa02ce64525c7d2b07
Evelyn Mylena <48720042+EvyMylena@users.noreply.github.com>
2019-08-23 09:43:14 -0300

    Create Pessoa.java

---
+++ Aula 23.08/Pessoa.java
@@ -0,0 +1,21 @@
+package meuprojeto;
+
+public class Pessoa {
+	private String nome;
+	private Telefone telefone;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Telefone getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+	
+}
