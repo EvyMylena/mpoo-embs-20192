fb23c0847683c38ba647c381c46bc8501d401377
Evelyn Mylena <48720042+EvyMylena@users.noreply.github.com>
2019-08-23 09:42:23 -0300

    Create Application.java

---
+++ Aula 23.08/Application.java
@@ -0,0 +1,23 @@
+package meuprojeto;
+
+public class Application {
+	public static void main(String[] args) {
+		//Imprimindo a pessoa e o seu telefone
+		Pessoa pessoa = new Pessoa();
+		Telefone telefone=new Telefone();
+		pessoa.setTelefone(telefone);
+		pessoa.setNome("Evelyn");
+		telefone.setDdd(81);
+		telefone.setNumero(999999);
+		telefone.setTipo(TipoTelefone.CELULAR);
+		imprimirPessoa(pessoa);
+	}
+
+	private static void imprimirPessoa(Pessoa pessoa) {
+		Telefone telefone=pessoa.getTelefone();
+		String nome = pessoa.getNome();
+		int ddd = pessoa.getTelefone().getDdd();
+		int numero = pessoa.getTelefone().getNumero();
+		System.out.println(nome + " (" + ddd+ ") " + numero + " - "+ pessoa.getTelefone().getTipo().getDescricao());
+	}
+}
